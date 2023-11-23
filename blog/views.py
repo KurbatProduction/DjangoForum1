197a2a2eed6e04ff888c78995f663601d3050d2d
Sergey Kurbatov <131869069+KurbatProduction@users.noreply.github.com>
2023-11-23 11:18:07 +0300

    add home_page.html & func tests for blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home_page(request):
+    return HttpResponse("""<html><title>Сайт Сергея Курбатова</title><h1>Сергей Курбатов</h1></html>""")
--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import unittest
 
 
@@ -12,13 +13,23 @@ class BasicInstallTest(unittest.TestCase):
     def test_home_page_title(self):
         self.browser.get("http://127.0.0.1:8000/")
         self.assertIn("Сайт Сергея Курбатова", self.browser.title)
-        # self.fail("Finish the test!")
 
     def test_home_page_header(self):
-        browser = self.browser.get("http://127.0.0.1:8000/")
-        header = browser.find_elements_by_tag_name('h1')[0]
+        self.browser.get("http://127.0.0.1:8000/")
+        header = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn("Сергей Курбатов", header)
-        # self.fail("Finish the test!")
+
+    def test_home_page_blog(self):
+        self.browser.get("http://127.0.0.1:8000/")
+        article_list = self.browser.find_element(By.CLASS_NAME, 'article_list')
+        self.assertTrue(article_list)
+
+    def test_home_page_articles_looks_correct(self):
+        self.browser.get("http://127.0.0.1:8000/")
+        article_title = self.browser.find_element(By.CLASS_NAME, 'article_title')
+        article_summary = self.browser.find_element(By.CLASS_NAME, 'article_summary')
+        self.assertTrue(article_title)
+        self.assertTrue(article_summary)
 
 
 if __name__ == "__main__":
