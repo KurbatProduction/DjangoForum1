73925c1034d14157bb0ca2844580950f86993654
Sergey Kurbatov <131869069+KurbatProduction@users.noreply.github.com>
2023-11-23 12:10:07 +0300

    change model Article

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,18 @@ from django.db import models
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
-    full_text = models.TextField()
     summary = models.CharField(max_length=255)
+    full_text = models.TextField()
+    author = models.CharField(max_length=50, default='')
+    source = models.CharField(max_length=255, default='')
     category = models.CharField(max_length=50)
     pubdate = models.DateTimeField()
     # is_published = models.BooleanField() #TODO
     # slug = models.CharField(max_length=255) #TODO
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
